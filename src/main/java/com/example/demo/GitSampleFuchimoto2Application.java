28701e32edfcf1b43cda6f05acb76bfe6ff32ef9
shiori.fuchimoto <shiori.fuchimoto@rakus.co.jp>
2017-06-14 10:50:53 +0900

    練習用に新規git-sample-fuchimoto2を作成した。

---
+++ src/main/java/com/example/demo/GitSampleFuchimoto2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleFuchimoto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleFuchimoto2Application.class, args);
+	}
+}
